62cd793c31a61df0dff76ea2c36517a8c8ec26b8
sararegalado <sararegalado@localhost>
2023-10-24 13:21:32 +0200

    toString

--- NatiShop/src/clases/Camiseta.java
+++ NatiShop/src/clases/Camiseta.java
@@ -9,5 +9,11 @@ public class Camiseta extends Articulo {
 	public Camiseta(String id, int unidades, double precio, Talla talla) {
 		super(id, unidades, precio, talla);
 	}
+
+	@Override
+	public String toString() {
+		return "Camiseta [id=" + id + ", unidades=" + unidades + ", precio=" + precio + ", talla=" + talla + "]";
+	}
+	
 	
 }
--- NatiShop/src/clases/Zapato.java
+++ NatiShop/src/clases/Zapato.java
@@ -9,6 +9,11 @@ public class Zapato extends Articulo {
 	public Zapato(String id, int unidades, double precio, Talla talla) {
 		super(id, unidades, precio, talla);
 	}
+
+	@Override
+	public String toString() {
+		return "Zapato [id=" + id + ", unidades=" + unidades + ", precio=" + precio + ", talla=" + talla + "]";
+	}
 	
 
 }
