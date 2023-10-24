19bf6b51f2c3c3d468b3d22c7da74761dccd5e7c
Janire Diaz <Janire Diaz@10.166.50.101>
2023-10-24 12:56:07 +0200

    janire

--- NatiShop/src/clases/Camiseta.java
+++ NatiShop/src/clases/Camiseta.java
@@ -1,29 +1,5 @@
 package clases;
 
 public class Camiseta extends Articulo {
-		private Talla talla;
-
-		public Camiseta() {
-			super();
-		}
-
-		public Camiseta(String id, int unidades, double precio, Talla talla) {
-			super(id, unidades, precio);
-			this.talla = talla;
-		}
-
-		public Talla getTalla() {
-			return talla;
-		}
-
-		public void setTalla(Talla talla) {
-			this.talla = talla;
-		}
-
-		@Override
-		public String toString() {
-			return "Camiseta [" + id + " " + unidades + " " + precio + " " + talla + "]";
-		}
-
-	}
+		
 }
