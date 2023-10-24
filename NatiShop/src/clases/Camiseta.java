72708c6a9a3b4458247b3105a66adb3722d9a58f
sararegalado <sararegalado@localhost>
2023-10-24 13:11:18 +0200

    Clases

--- NatiShop/src/clases/Camiseta.java
+++ NatiShop/src/clases/Camiseta.java
@@ -1,5 +1,13 @@
 package clases;
 
 public class Camiseta extends Articulo {
+
+	public Camiseta() {
+		super();
+	}
+
+	public Camiseta(String id, int unidades, double precio, Talla talla) {
+		super(id, unidades, precio, talla);
+	}
 	
 }
--- NatiShop/src/clases/Zapato.java
+++ NatiShop/src/clases/Zapato.java
@@ -2,4 +2,13 @@ package clases;
 
 public class Zapato extends Articulo {
 
+	public Zapato() {
+		super();
+	}
+
+	public Zapato(String id, int unidades, double precio, Talla talla) {
+		super(id, unidades, precio, talla);
+	}
+	
+
 }
