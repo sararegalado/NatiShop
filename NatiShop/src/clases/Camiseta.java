0f748e32e065361f1304ec0a2e97b67e10bbc68b
Janire Diaz <Janire Diaz@10.164.54.103>
2023-10-31 21:50:39 +0100

    Merge branch 'master' of https://github.com/sararegalado/NatiShop.git

--- NatiShop/src/clases/Camiseta.java
+++ NatiShop/src/clases/Camiseta.java
@@ -9,20 +9,9 @@ public class Camiseta extends Articulo {
 	public Camiseta(String id, int unidades, double precio, Talla talla) {
 		super(id, unidades, precio, talla);
 	}
-<<<<<<< HEAD
-	
-=======
 
-
->>>>>>> branch 'master' of https://github.com/sararegalado/NatiShop.git
 	@Override
 	public String toString() {
 		return "Camiseta [id=" + id + ", unidades=" + unidades + ", precio=" + precio + ", talla=" + talla + "]";
 	}
-<<<<<<< HEAD
 }
-=======
-
-	
-}
->>>>>>> branch 'master' of https://github.com/sararegalado/NatiShop.git
